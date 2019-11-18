90c20fde845dd7d0107370afed0142b34f2505b5
D-Kleck <David@Davids-MacBook-Pro.local>
2019-11-18 16:35:20 -0700

    getting help

--- week3/src/app.spec.ts
+++ week3/src/app.spec.ts
@@ -1,2 +1,23 @@
 import { expect } from 'chai';
 import 'mocha';
+import { Party } from './app';
+
+describe('Test for app.ts', () => {
+    const guest = new Party('John', 30, 'Bill', 'Jerry');
+
+    it('should return true', () => {
+        expect(guest.isAdult()).to.equal(true);
+    });
+
+    it('should return true', () => {
+        expect(guest.getAge()).to.equal(true);
+    });
+
+    it('should return Bill', () => {
+        expect(guest.getPlus1()).to.equal('John');
+    });
+
+    it('should return an array of Bill and Jerry', () => {
+        expect(guest.getPluses()).to.equal('Bill');
+    });
+});
