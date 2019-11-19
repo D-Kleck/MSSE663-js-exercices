b9c223e6f3ccee03128ee5f7a37d80679d638308
D-Kleck <David@Davids-MacBook-Pro.local>
2019-11-18 17:47:06 -0700

    observables part of assignment

--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,1 +1,10 @@
-// Source code here
+import {of} from 'rxjs';
+import {map} from 'rxjs/operators';
+
+export const data = ['Dog', 'Cat', 'Fish'];
+
+export const source = of(data);
+
+source.subscribe(list => {
+	console.log(list);
+});
