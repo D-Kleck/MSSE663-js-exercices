3f31f91f7578d30ae7d07c863be06e9504a5f1d2
D-Kleck <David@Davids-MacBook-Pro.local>
2019-11-18 17:47:06 -0700

    observables part of assignment

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,2 +1,11 @@
+import {source, data} from './observable';
 import { expect } from 'chai';
 import 'mocha';
+
+describe('Testing datastream', () => {
+    it('should ouput source stream List of Animals', () => {
+		source.subscribe(list => {
+			expect(list).to.eql(data);
+		});
+	});
+})
