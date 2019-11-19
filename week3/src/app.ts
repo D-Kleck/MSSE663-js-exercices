7f371ab732bc26d700d8483c17f364cbaa14059a
D-Kleck <David@Davids-MacBook-Pro.local>
2019-11-18 17:27:00 -0700

    fixed issue with js

--- week3/src/app.ts
+++ week3/src/app.ts
@@ -4,14 +4,13 @@ export interface Guest {
     plus1: string;
     plus2: string;
     isAdult: () => boolean;
-    getPlus1: () => string;
     getAge: () => boolean;
     getPluses: () => Array<string>;
 }
 
 
 export class Party implements Guest {
-    extras = ['dog'];
+    extras = [''];
 
     constructor(public name:string, public age: number, public plus1:string, public plus2:string) {
         this.name = name;
@@ -19,26 +18,19 @@ export class Party implements Guest {
         this.plus1 = plus1;
         this.plus2 = plus2;
         this.extras = Array<string>(plus1, plus2);
-        this.extras.push(this.plus1, this.plus2);
+
 
     }
 
     getAge(): boolean {
-        if(this.age >= 21) {return true;}
-        else {return false;}
+        return this.age >=21;
     }
 
     isAdult(): boolean {
-        if(this.age >= 21) {return true;}
-        else {return false;}
-    }
-
-    getPlus1(): string {
-        return this.plus1;
+        return this.age >=21;
     }
 
     getPluses(): Array<string> {
-        this.extras;
         return this.extras;
     }
 
